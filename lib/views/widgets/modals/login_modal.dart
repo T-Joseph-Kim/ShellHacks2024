966db89f301d3bbf3bbbf51a8f135b0e9e5328a1
Taebok Joseph Kim <josephkim7104@gmail.com>
2024-09-28 14:46:58 -0400

    Camera Page and some styling

--- lib/views/widgets/modals/login_modal.dart
+++ lib/views/widgets/modals/login_modal.dart
@@ -10,9 +10,9 @@ class LoginModal extends StatelessWidget {
       children: [
         Container(
           width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height * 85 / 100,
+          height: MediaQuery.of(context).size.height * 70/ 100,
           padding: EdgeInsets.only(left: 16, right: 16, bottom: 32, top: 16),
-          decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20))),
+          decoration: BoxDecoration(color: AppColor.primary, borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20))),
           child: ListView(
             shrinkWrap: true,
             padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
@@ -32,12 +32,12 @@ class LoginModal extends StatelessWidget {
                 margin: EdgeInsets.only(bottom: 24),
                 child: Text(
                   'Login',
-                  style: TextStyle(color: Colors.black, fontSize: 22, fontWeight: FontWeight.w700, fontFamily: 'inter'),
+                  style: TextStyle(color: AppColor.secondary, fontSize: 22, fontWeight: FontWeight.w700, fontFamily: 'inter'),
                 ),
               ),
               // Form
-              CustomTextField(title: 'Email', hint: 'youremail@email.com'),
-              CustomTextField(title: 'Password', hint: '**********', obsecureText: true, margin: EdgeInsets.only(top: 16)),
+              CustomTextField(title: 'Email', hint: 'e.g. foodlover@email.com'),
+              CustomTextField(title: 'Password', hint: 'e.g. Ilovetoeatfood1', obsecureText: true, margin: EdgeInsets.only(top: 16)),
               // Log in Button
               Container(
                 margin: EdgeInsets.only(top: 32, bottom: 6),
@@ -48,26 +48,26 @@ class LoginModal extends StatelessWidget {
                     Navigator.of(context).pop();
                     Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => PageSwitcher()));
                   },
-                  child: Text('Login', style: TextStyle(color: AppColor.secondary, fontSize: 16, fontWeight: FontWeight.w600, fontFamily: 'inter')),
+                  child: Text('Login', style: TextStyle(color: AppColor.primary, fontSize: 16, fontWeight: FontWeight.w600, fontFamily: 'inter')),
                   style: ElevatedButton.styleFrom(
                     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                    primary: AppColor.primarySoft,
+                    primary: AppColor.secondary,
                   ),
                 ),
               ),
               TextButton(
                 onPressed: () {},
                 style: TextButton.styleFrom(
-                  primary: Colors.white,
+                  primary: AppColor.secondary,
                 ),
                 child: RichText(
                   text: TextSpan(
                     text: 'Forgot your password? ',
-                    style: TextStyle(color: Colors.grey),
+                    style: TextStyle(color: AppColor.secondary),
                     children: [
                       TextSpan(
                           style: TextStyle(
-                            color: AppColor.primary,
+                            color: AppColor.secondary,
                             fontWeight: FontWeight.w700,
                             fontFamily: 'inter',
                           ),
--- lib/views/widgets/modals/register_modal.dart
+++ lib/views/widgets/modals/register_modal.dart
@@ -13,7 +13,7 @@ class RegisterModal extends StatelessWidget {
           width: MediaQuery.of(context).size.width,
           height: MediaQuery.of(context).size.height * 85 / 100,
           padding: EdgeInsets.only(left: 16, right: 16, bottom: 32, top: 16),
-          decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20))),
+          decoration: BoxDecoration(color: AppColor.primarySoft, borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20))),
           child: ListView(
             shrinkWrap: true,
             padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
@@ -32,12 +32,12 @@ class RegisterModal extends StatelessWidget {
               Container(
                 margin: EdgeInsets.only(bottom: 24),
                 child: Text(
-                  'Get Started',
-                  style: TextStyle(color: Colors.black, fontSize: 22, fontWeight: FontWeight.w700, fontFamily: 'inter'),
+                  'Join Us',
+                  style: TextStyle(color: AppColor.secondary, fontSize: 22, fontWeight: FontWeight.w700, fontFamily: 'inter'),
                 ),
               ),
               // Form
-              CustomTextField(title: 'Email', hint: 'youremail@email.com'),
+              CustomTextField(title: 'Email', hint: 'e.g. foodlover@email.com'),
               CustomTextField(title: 'Full Name', hint: 'Your Full Name', margin: EdgeInsets.only(top: 16)),
               CustomTextField(title: 'Password', hint: '**********', obsecureText: true, margin: EdgeInsets.only(top: 16)),
               CustomTextField(title: 'Retype Password', hint: '**********', obsecureText: true, margin: EdgeInsets.only(top: 16)),
@@ -51,10 +51,10 @@ class RegisterModal extends StatelessWidget {
                     Navigator.of(context).pop();
                     Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => PageSwitcher()));
                   },
-                  child: Text('Register', style: TextStyle(color: AppColor.secondary, fontSize: 16, fontWeight: FontWeight.w600, fontFamily: 'inter')),
+                  child: Text('Register', style: TextStyle(color: AppColor.primary, fontSize: 16, fontWeight: FontWeight.w600, fontFamily: 'inter')),
                   style: ElevatedButton.styleFrom(
                     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                    primary: AppColor.primarySoft,
+                    primary: AppColor.secondary,
                   ),
                 ),
               ),
